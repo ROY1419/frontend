9a4e490e69698363cc1b5feb11cfcc628a496ebf
ROY1419 <royshubham471@gmail.com>
2022-08-19 10:33:25 +0530

    major-project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/main/Header';
+import Admin from './components/admin';
+import Main from './components/main';
+import User from './components/user';
+import Login from './components/main/Login';
+import Signup from './components/main/Signup';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DashBoard from './components/admin/DashBoard';
+import Home from './components/admin/Home';
+import MangeUser from './components/admin/MangeUser';
+import Profile from './components/admin/Profile';
+import ResetPassword from './components/admin/ResetPassword';
+import ContactUs from './components/main/ContactUs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <BrowserRouter>
+    {/* <Header /> */}
+    <Routes>
+      <Route element={<Admin />} path="admin">
+        <Route path='dashboard' element={<DashBoard />} />
+        <Route path='header' element={<Header />} />
+        <Route path='home' element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='mangeruser' element={<MangeUser />} />
+        <Route path='profile' element={<Profile />} />
+        <Route path='reset-password' element={<ResetPassword />} />
+        <Route path='signup' element={<Signup />} />
+      </Route>
+      <Route element={<Main />} path="main">
+        <Route path="signup" element={<Signup />} />
+        <Route path='contactus' element={<ContactUs />} />
+        <Route path='header' element={<Header />}/>
+        <Route path='home' element={<Home/>}/>
+        <Route path='login' element={<Login/>} />
+        <Route path='reset-password' element={<ResetPassword/>} />
+      </Route>
+      <Route element={<User />} path="user" >
+      <Route path='header' element={<Header/>} />
+      <Route path='home' element={<Home/>} />
+      <Route path='login' element={<Login/>} />
+      <Route path='reset-password' element={<ResetPassword/>} />
+      <Route path='signup' element={<Signup/>} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
+   </div>
   );
 }
 
---
+++ src/components/admin/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
+
+const index = () => {
+  return (
+    <div>
+        <Header />
+        <Outlet />
+    </div>
+  )
+}
+
+export default index
